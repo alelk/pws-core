ca20de3168bcc50eda8f7399d777f204ac3980dc
Alex Elkin <alelkdev@gmail.com>
2025-12-01 03:22:44 +0300

    feat(server-transport): Add book routes tests.

--- api/contract/src/commonMain/kotlin/book/songnumber/ReplaceAllBookSongNumbersResult.kt
+++ api/contract/src/commonMain/kotlin/book/songnumber/ReplaceAllBookSongNumbersResult.kt
@@ -1,7 +1,9 @@
-package book.songnumber
+package io.github.alelk.pws.api.contract.book.songnumber
 
+import kotlinx.serialization.Serializable
 import io.github.alelk.pws.api.contract.book.songnumber.SongNumberLinkDto
 
+@Serializable
 data class ReplaceAllBookSongNumbersResult(
   val created: List<SongNumberLinkDto>,
   val updated: List<SongNumberLinkDto>,
