49331a10c61b48f65e34409aee9520bd9bc96260
Alex Elkin <alelkdev@gmail.com>
2025-12-18 18:40:31 +0300

    fix: Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
   alias(libs.plugins.kotlinMultiplatform) apply false
   alias(libs.plugins.kotlinSerialization) apply false
   alias(libs.plugins.androidLibrary) apply false
-  id("com.google.devtools.ksp") version "${libs.versions.kotlin.get()}-${libs.versions.ksp.get()}" apply false
+  id("com.google.devtools.ksp") version "${libs.versions.ksp.get()}" apply false
 }
 
 val androidSdkVersion by extra(35)
